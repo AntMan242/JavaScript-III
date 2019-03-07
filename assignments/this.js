2416d4b21abb3c5cc8de381e63f659aeb2bcb467
anthony <anthony24.kwhomes@gmail.com>
2019-03-06 16:19:23 -0800

    Still working on the project.

--- assignments/this.js
+++ assignments/this.js
@@ -1,4 +1,4 @@
-/* The for principles of "this";
+/* The four principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
 * 1. 
@@ -12,15 +12,45 @@
 // Principle 1
 
 // code example for Window Binding
+    function sayHi(name) {
+        console.log('Hello' + name);
+        console.log(this);
+    }
 
-// Principle 2
+    sayHi('Anthony');
+
+Principle 2
 
 // code example for Implicit Binding
+const user = {
+    name: 'Anthony',
+    age: 'Grown Man',
+    user.sayHi() {
+        console.log(`Hello, my name is ${this.name}`)
+      }
+}
 
 // Principle 3
 
 // code example for New Binding
+function user(name, age) {
+    this.name
+    this.age
+}
+const me = new user('Anthony', 4);
 
 // Principle 4
 
 // code example for Explicit Binding
+function sayHi(a, b, c) {
+    console.log(`Hello, my name is ${this.name} and i know ${a}, ${b}, ${c}`)
+
+}
+
+const user = {
+    name: 'Anthony',
+    age: 4
+}
+
+const languages = ['Up', 'down', 'right']
+sayHi.call(user, languages[0], languages[1], languages[2]);
